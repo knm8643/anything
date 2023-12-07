fc6dca1a7e78f9dd0bf3f8c61d8bf5b16def1f12
DESKTOP-8TILB7O\1 <yuuurim2205@gmail.com>
2023-12-07 15:36:48 +0900

    이력서 퍼블 수정

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,105 +1,266 @@
 <template>
   <div class="hello">
     <header class="profil">
-      <div class="profile-picture">
-        <img src="../assets/images/Photo.png" alt="이충재의 프로필 사진" />
-      </div>
-      <div class="description">
-        <p style="color: #1465b6; font-size: 30px">이충재</p>
-        <p>프론트엔드 개발자</p>
-        <p style="font-size: 11pt">
-          010-5696-1909 / knm8643@nate.com /
-          <a href="https://github.com/knm8643"
-            >github <i class="fa-brands fa-github"></i
-          ></a>
-          /
-        </p>
-        <span>
-          최신 웹 트렌드를 반영하기 위해<br />
-          다양한 프론트엔드 프레임워크와 타입스크립트를 활용하며 사용자 중심의
-          개발에 지향합니다.<br />
-          만족도를 높이고 효율적인 개발과 사용자 경험 개선을 <br />
-          추구합니다.
-        </span>
-      </div>
-    </header>
-    <div class="experience">
-      <h4>경력 및 교육</h4>
-      <table>
-        <thead>
-          <tr>
-            <th style="width: 20%">근무 기간</th>
-            <th style="width: 20%">회사명</th>
-            <th style="width: 60%">주요 업무 내용</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>2022.04 - 2023.05</td>
-            <td>LF<br />트라이씨클</td>
-            <td style="text-align: left; padding: 20px">
-              • 하프클럽 보리보리 신규기능 개발<br />
-              • 사이트 기능 유지보수 <br />
-              • 데이터스토리 집계 결과 수집 <br />
-              • 매출 코드 집계
-            </td>
-          </tr>
-          <tr>
-            <td>2021.09 - 2022.02</td>
-            <td>KH<br />정보교육원</td>
-            <td style="text-align: left; padding: 20px">
-              • 웹 개발자 양성과정 교육 이수 <br />
-              - Java, Spring, Oracle, JavaScript, JSP, Tomcat
-            </td>
-          </tr>
-          <tr>
-            <td>2019.03 - 2021.08</td>
-            <td>서울특별시사회복지<br />협의회</td>
-            <td style="text-align: left; padding: 20px">
-              • 사이트 리뉴얼 의뢰<br />
-              - 프레임워크 선정 및 ERP 구조 설계, 리뉴얼 사이트 구축 계획
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-    <div class="description02">
-      <h4>Stack</h4>
-      <div class="tech_text">
-        <div id="techList">
-          <div>
-            <i class="fa-brands fa-java"></i> Java , Spring, and SpringBoot<br />
-            <i class="fa-brands fa-js"></i> JSP, JavaScript, Vue3, typeScript<br />
-            <i class="fa-brands fa-angular"></i> AngularJs<br />
-            <font-awesome-icon icon="database" /> Oracle, MySQL, and Dbeaver<br />
-            <i class="fa-brands fa-github"></i> GitHub and GitLab
-          </div>
+      <article class="description">
+        <section>
+          <h1>안녕하세요, 개발자 이충재 라고 합니다<em>.</em></h1>
+        </section>
+        <div>
+          <p>
+            안녕하세요 저는 2년차 프론트엔드 개발자로<br />
+            최신 웹 트렌드를 반영하기 위해<br />
+            다양한 프론트엔드 프레임워크와 타입스크립트를 활용하며,<br />
+            사용자 중심의개발에 지향합니다.<br />
+            만족도를 높이고 효율적인 개발과 사용자 경험 개선을 <br />
+            추구합니다.
+          </p>
         </div>
-      </div>
-    </div>
-    <div class="portfolio">
-      <h4>Projects</h4>
-      <div class="probox">
+      </article>
+    </header>
+    <article class="experience">
+      <h1>Work Experience<em>.</em></h1>
+      <section>
+        <div class="row-left">
+          <h2>트라이씨클<em>LF.</em></h2>
+          <p>Frontend Developer</p>
+          <p>2022.04 - 2023.05</p>
+          <p>온라인쇼핑몰 개발/ 운영</p>
+        </div>
+        <div class="row-right">
+          <h3>IT개발본부 프론트개발</h3>
+          <ul>
+            <li>자사몰(하프클럽, 보리보리) 서비스 고도화 및 개선작업</li>
+            <li>쇼핑몰 서비스에서 구동되는 내부 api 및 웹개발</li>
+            <li>AngularJS 유지보수</li>
+            <li>커머스플랫폼 기반의 비즈니스 로직 구현 및 성능 최적화</li>
+            <li>레거시코드 리팩터링</li>
+            <li>하프클럽, 보리보리 자사몰 신규기능개발</li>
+            <li>프레임워크 구조 변경 (angular → vue3)</li>
+            <li>데이터스토리 집계 결과 수집</li>
+            <li>매출 코드 집계</li>
+          </ul>
+        </div>
+      </section>
+      <section>
+        <div class="row-left">
+          <h2>정보교육원<em>KH.</em></h2>
+          <p>교육 이수</p>
+          <p>2021.09 - 2022.02</p>
+        </div>
+        <div class="row-right">
+          <h3>개발 전문 교육 과정</h3>
+          <ul>
+            <li>웹 개발자 양성과정 교육 이수</li>
+            <li>프로젝트 리딩 및 다양한 팀과의 협업 경험</li>
+            <li>HTML, CSS, JAVASCRIPT,JQUERY 등과 기초적인 IT 지식 교육</li>
+            <li>Java</li>
+            <li>Spring</li>
+            <li>Oracle</li>
+            <li>JavaScript</li>
+            <li>JSP</li>
+            <li>Tomcat</li>
+          </ul>
+        </div>
+      </section>
+      <section>
+        <div class="row-left">
+          <h2>서울특별시사회복지협의회<em>.</em></h2>
+          <p>Frontend Developer</p>
+          <p>2019.03 - 2021.08</p>
+          <p>사이트 개발/ 운영</p>
+        </div>
+        <div class="row-right">
+          <h3>개발 설계 구축</h3>
+          <ul>
+            <li>사이트 리뉴얼 의뢰</li>
+            <li>프레임워크 선정 및 ERP 구조 설계, 리뉴얼 사이트 구축 계획</li>
+          </ul>
+        </div>
+      </section>
+    </article>
+    <article class="skills">
+      <h1>Skills<em>.</em></h1>
+      <section>
+        <div class="row-center">
+          <h2>Communication<em>.</em></h2>
+          <ul>
+            <li>
+              <h4>지적 겸손함을 유지하기 위해 노력합니다.</h4>
+            </li>
+            <li>
+              <h4>커머스 플랫폼에 대한 폭넓은 이해와 운영 경험</h4>
+              <p>
+                - 여러부서들과 협업하면서 설계부터 개발 일정 까지 주도적으로
+                하면서 정해진 기간안에 개발을 완료한 경험이 있습니다.
+              </p>
+            </li>
+            <li>
+              <h4>
+                모르는 기술이 있더라도 필요하다면 능동적으로 찾아보고 학습하여
+                업무에 적극적으로 활용합니다.
+              </h4>
+            </li>
+          </ul>
+        </div>
+      </section>
+      <section>
+        <div class="row-center">
+          <h2>Frontend<em>.</em></h2>
+          <ul>
+            <li>
+              <h4>JavaScript</h4>
+              <p>
+                - ES2015를 포함한 JavaScript 문법에 능숙하며, ECMAScript의 최신
+                명세와 동향을 꾸준히 살피고 학습합니다.
+              </p>
+              <p>
+                - 웹 서비스를 설계할 때 겉으로 드러나는 시각적인 영역에만
+                집중하지 않고,의미에 맞는 시맨틱 요소를 사용하고 웹 표준과 웹
+                접근성을 고려한 SEO맞춤 설계하는 데 많은 신경을 씁니다.
+              </p>
+            </li>
+            <li>
+              <h4>Angular.js</h4>
+              <p>- 주로 Angular를 사용하여 회사내 프로젝트를 개발하였습니다.</p>
+              <p>- Angular 기반 Typescript도 업무에 활용 가능합니다.</p>
+              <p>
+                - 모듈화와 컴포넌트 기반의 설계에 익숙하여 복잡한 웹 어플리케이
+                션을 개발할수있습니다.
+              </p>
+            </li>
+            <li>
+              <h4>Vue.js</h4>
+              <p>
+                - 협업사이트인 블로그 만드는 블로그 라는 프로젝트를
+                제작하고있습니다.
+              </p>
+            </li>
+            <li>
+              <h4>React.js</h4>
+              <p>
+                - 재사용 로직을 분리하기 위해 커스텀 Hooks을 자유롭게 활용하고,
+                다양한 최적화 기법과 컴포넌트 디자인 패턴을 사용기위해 공부 하고
+                있습니다.
+              </p>
+            </li>
+          </ul>
+        </div>
+      </section>
+      <section>
+        <div class="row-center">
+          <h2>back<em>.</em></h2>
+          <ul>
+            <li>
+              <h4>Java</h4>
+            </li>
+            <li>
+              <h4>Spring and SpringBoot</h4>
+              <p>
+                - 다양한 프로젝트 경험으로 안정적이고 확장성 있는 애플리케이션을
+                개발할 수 있습니다.
+              </p>
+            </li>
+            <li>
+              <h4>Oracle</h4>
+            </li>
+            <li>
+              <h4>MySQL, and Dbeaver</h4>
+              <p>
+                - 데이터베이스를 효율적으로 설계하고 관리할 수 있으며,성능
+                최적화 및 쿼리 튜닝에 경험이 있습니다.
+              </p>
+            </li>
+          </ul>
+        </div>
+      </section>
+      <section>
+        <div class="row-center">
+          <h2>그 외<em>.</em></h2>
+          <ul>
+            <li>
+              <h4>GitHub, GitLab</h4>
+              <p>
+                - Git을 사용한 워크플로우에 익숙하며, 변경 사항을 추적하기
+                쉽도록 이력을 자세하게 기록하고, 커밋 히스토리를 깔끔하게
+                유지하기 위해 Interactive Rebase를 적극적으로 활용합니다.
+              </p>
+              <p>
+                - 협업 및 버전 관리를 효율적으로 수행할 수 있습니다. 오픈 소스
+                프로젝트에 참여한 경험이 있어, 다양한 개발자들과의 협업에
+                능숙합니다.
+              </p>
+            </li>
+            <li>
+              <h4>업무</h4>
+              <p>- Slack, Gmail 등 업무용 메신저를 사용한 소통에 익숙합니다.</p>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </article>
+    <article class="portfolio">
+      <h1>Projects<em>.</em></h1>
+      <section class="probox">
+        <div class="row-left">
+          <h2>프로젝트제목</h2>
+          <p>프로젝트 설명프로젝트 설명</p>
+        </div>
         <router-link to="/pacman" class="portfolio-link">
           <div class="portfolio-box">
             <img src="../assets/images/back.webp" alt="포트폴리오 이미지" />
             <div class="overlay">포트폴리오 보기</div>
           </div>
         </router-link>
+      </section>
+      <section class="probox">
+        <div class="row-left">
+          <h2>프로젝트제목</h2>
+          <p>프로젝트 설명프로젝트 설명</p>
+        </div>
         <router-link to="/anyThingPage" class="portfolio-link">
           <div class="portfolio-box">
             <img src="../assets/images/back.webp" alt="포트폴리오 이미지" />
             <div class="overlay">포트폴리오 보기</div>
           </div>
         </router-link>
+      </section>
+      <section class="probox">
+        <div class="row-left">
+          <h2>프로젝트제목</h2>
+          <p>프로젝트 설명프로젝트 설명</p>
+        </div>
         <router-link to="/" class="portfolio-link">
           <div class="portfolio-box">
             <img src="../assets/images/img-ready.png" alt="포트폴리오 이미지" />
             <div class="overlay">포트폴리오 보기</div>
           </div>
         </router-link>
-      </div>
-    </div>
+      </section>
+    </article>
+    <article class="contact">
+      <h1>Contact<em>.</em></h1>
+      <section>
+        <p>
+          저의 변화와 성장은 그동안의 다양한 경험들을 통해 쌓아온 것이라고
+          생각합니다<em>.</em><br />
+          앞으로도 끊임없는 도전과 학습을 통해 더 나은 전문가가 되기 위해
+          노력하겠습니다. <br />
+          감사합니다<em>.</em>
+        </p>
+        <ul>
+          <li>
+            <h4>✉</h4>
+            <p>knm8643@nate.com</p>
+          </li>
+          <li>
+            <h4><i class="fa-brands fa-github"></i></h4>
+            <p>
+              <a href="https://github.com/knm8643">깃허브</a>
+            </p>
+          </li>
+        </ul>
+      </section>
+    </article>
   </div>
 </template>
 
@@ -120,142 +281,307 @@ export default class HelloWorld extends Vue {
 </script>
 
 <style lang="scss">
+$mobile-width: 664px;
 @font-face {
-  font-family: "TheJamsil5Bold";
-  src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2302_01@1.0/TheJamsil5Bold.woff2")
+  font-family: "TTWanjunuricheR";
+  src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2307-1@1.1/TTWanjunuricheR.woff2")
+    format("woff2");
+  font-weight: normal;
+  font-style: normal;
+}
+@font-face {
+  font-family: "TTWanjudaedunsancheB";
+  src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2304-2@1.0/TTWanjudaedunsancheB.woff2")
     format("woff2");
   font-weight: 700;
   font-style: normal;
 }
 
 .profil {
-  display: flex;
-  align-items: center;
-  justify-content: space-evenly;
-  margin-top: 7%;
-  font-family: "TheJamsil5Bold", sans-serif;
-  border-top: 1px solid #eee;
-  padding: 20px 0;
-  border-bottom: 1px solid #eee;
-  font-size: 20px;
-  .profile-picture {
-    width: 200px;
-    height: 300px;
-    border-radius: 50%;
-    overflow: hidden;
-    img {
-      width: 100%;
-      height: 100%;
-    }
+  animation: fadein 3s ease-in-out;
+  padding: 2rem;
+  @media (max-width: $mobile-width) {
+    padding: 2rem;
   }
+
   .description {
-    text-align: left;
-    cursor: pointer;
-    color: #083058;
-    padding: 50px;
-    span {
-      line-height: 0.5em;
-      vertical-align: 0.5em;
-      &:hover {
-        border-bottom: 0.4em solid transparent;
-        -moz-border-image: -moz-linear-gradient(left, #a8dbed 0%, #e291e6 100%);
-        -webkit-border-image: -webkit-linear-gradient(
-          left,
-          #a8dbed 0%,
-          #e291e652 100%
-        );
-        border-image: linear-gradient(to right, #a8dbed 0%, #e291e652 100%);
-        border-image-slice: 1;
+    margin: 6rem auto;
+    color: #000;
+    section {
+      display: flex;
+      h1 {
+        font-family: "TTWanjudaedunsancheB";
+        font-size: 5rem;
+        width: 62%;
+        text-align: left;
+        margin-bottom: 1rem;
+        @media (max-width: $mobile-width) {
+          font-size: 2.2rem;
+          width: 100%;
+        }
+        em {
+          color: #00d2ff;
+          font-style: normal;
+        }
+      }
+    }
+    div {
+      font-family: "TTWanjunuricheR";
+      text-align: left;
+      line-height: 1.5;
+      font-size: 1.8rem;
+      p {
+        margin-top: 2rem;
       }
     }
   }
 }
 .experience {
-  font-family: "TheJamsil5Bold";
-  color: #083058;
-  border-bottom: 1px solid #eee;
-  padding: 50px 0;
-  font-size: 20px;
-  h4 {
-    border-bottom: 3px solid #083058;
-    padding-bottom: 10px;
-    text-align: left;
-    font-size: 30px;
-    width: 160px;
-  }
-  table {
-    width: 100%;
-    thead {
-      background: #083058;
-      color: #fff;
-      line-height: 2;
+  padding: 8rem 2rem;
+  font-family: "TTWanjunuricheR";
+  h1 {
+    display: inline-block;
+    padding: 0.2rem 1rem;
+    background-color: #00d2ff;
+    font-size: 3rem;
+    em {
+      color: #4e56e8;
+      font-style: normal;
     }
-    tbody {
-      background: #eee;
-      line-height: 2.5;
-      font-size: 17px;
+  }
+  section {
+    display: flex;
+    padding: 2rem 0;
+    border-bottom: 1px solid rgb(224, 226, 231);
+    .row-left {
+      width: 300px;
+      h2 {
+        padding-bottom: 1rem;
+        font-size: 1.4rem;
+        margin: 0;
+        em {
+          font-style: normal;
+          color: #4e56e8;
+        }
+      }
+      p {
+        padding-bottom: 0.5rem;
+        color: #7e7979;
+        margin: 0;
+        @media (max-width: $mobile-width) {
+          font-size: 0.8rem;
+        }
+      }
+    }
+    .row-right {
+      h3 {
+        padding-bottom: 1rem;
+        font-size: 1.4rem;
+        margin: 0;
+      }
+      ul {
+        margin: 0;
+        padding: 0 1rem;
+        li {
+          padding: 0 0 1rem 0;
+          &::marker {
+            list-style: disc;
+            color: #4e56e8;
+          }
+        }
+      }
     }
   }
 }
-.description02 {
-  font-family: "TheJamsil5Bold";
-  color: #083058;
-  border-bottom: 1px solid #eee;
-  padding: 50px 0;
-  font-size: 20px;
-  h4 {
-    border-bottom: 3px solid #083058;
-    padding-bottom: 10px;
-    text-align: left;
-    font-size: 30px;
-    width: 85px;
+.skills {
+  padding: 8rem 2rem;
+  font-family: "TTWanjunuricheR";
+  h1 {
+    display: inline-block;
+    padding: 0.2rem 1rem;
+    background-color: #00d2ff;
+    font-size: 3rem;
+    em {
+      color: #4e56e8;
+      font-style: normal;
+    }
   }
-  .tech_text {
-    line-height: 6;
-    padding: 20px;
-    background: #eee;
-    .techList {
-      letter-spacing: 2px;
+  section {
+    padding: 2rem 0;
+    border-bottom: 1px solid rgb(224, 226, 231);
+    h2 {
+      line-height: 0.3;
+      border-bottom: 6px solid #00d2ff52;
+      display: inline-block;
+      font-size: 2rem;
+      margin: 0;
+      em {
+        color: #4e56e8;
+        font-style: normal;
+      }
+    }
+    ul {
+      padding: 0 1rem;
+      margin: 0;
+      li {
+        margin: 0;
+        &::marker {
+          list-style: disc;
+          color: #4e56e8;
+        }
+        h4 {
+          margin-bottom: 1rem;
+        }
+        p {
+          line-height: 1.5;
+          padding-bottom: 0.5rem;
+          color: #7e7979;
+        }
+      }
     }
   }
 }
 .portfolio {
-  font-family: "TheJamsil5Bold";
-  color: #083058;
-  border-bottom: 1px solid #eee;
-  padding: 50px 0;
-  font-size: 20px;
-  h4 {
-    border-bottom: 3px solid #083058;
-    padding-bottom: 10px;
-    text-align: left;
-    font-size: 30px;
-    width: 123px;
+  padding: 8rem 2rem;
+  font-family: "TTWanjunuricheR";
+  h1 {
+    display: inline-block;
+    padding: 0.2rem 1rem;
+    background-color: #00d2ff;
+    font-size: 3rem;
+    em {
+      color: #4e56e8;
+      font-style: normal;
+    }
   }
   .probox {
-    background: #eee;
-    display: grid;
-    justify-content: center;
-    padding: 35px;
-    a {
-      display: inline-block;
-      padding: 15px;
-      margin-bottom: 20px;
-      background: #fff;
+    display: flex;
+    justify-content: space-evenly;
+    padding: 2rem 0;
+    border-bottom: 1px solid rgb(224, 226, 231);
+    @media (max-width: $mobile-width) {
+      display: block;
+      padding: 7rem 0;
+    }
+    .row-left {
+      width: 350px;
+      h2 {
+        line-height: 0.3;
+        border-bottom: 6px solid rgba(0, 210, 255, 0.3215686275);
+        display: inline-block;
+        font-size: 2rem;
+      }
+    }
+    .portfolio-link {
       text-decoration: none;
-      color: #083058;
+      font-family: "TTWanjunuricheR";
+      color: #000;
+      font-weight: bold;
+      padding-bottom: 2rem;
+      @media (max-width: $mobile-width) {
+        color: #4e56e8;
+        text-decoration: underline;
+        text-underline-offset: 5px;
+      }
       &:hover {
-        box-shadow: 0 0px 5px #a5c1dc, 0 6px 16px #c1d6eb;
-        background: #eee;
+        text-decoration: underline;
+        text-underline-offset: 5px;
+        color: #4e56e8;
       }
       .portfolio-box {
-        width: 300px;
+        width: 360px;
+        height: 250px;
+        @media (max-width: $mobile-width) {
+          width: 100%;
+        }
         img {
           width: 100%;
           height: 100%;
         }
+        .overlay {
+          font-size: 1.125rem;
+          line-height: 2em;
+          text-align: center;
+          &:after {
+            margin-left: 0.15em;
+            font-weight: 400;
+            content: "↗️";
+          }
+        }
       }
     }
+  }
+}
+.contact {
+  padding: 8rem 2rem;
+  font-family: "TTWanjunuricheR";
+  h1 {
+    display: inline-block;
+    padding: 0.2rem 1rem;
+    background-color: #00d2ff;
+    font-size: 3rem;
+    em {
+      color: #4e56e8;
+      font-style: normal;
+    }
+  }
+  section {
+    padding: 2rem 0;
+    border-bottom: 1px solid rgb(224, 226, 231);
+    p {
+      font-family: "TTWanjunuricheR";
+      text-align: left;
+      line-height: 1.5;
+      font-size: 1.8rem;
+      em {
+        color: #4e56e8;
+        font-style: normal;
+      }
+    }
+    ul {
+      padding: 0 1rem;
+      margin: 0;
+      li {
+        margin: 0;
+        &::marker {
+          list-style: disc;
+          color: #4e56e8;
+        }
+        h4 {
+          margin: 0;
+          font-size: 3rem;
+        }
+        p {
+          line-height: 1.5;
+          padding-bottom: 0.5rem;
+          color: #4e56e8;
+          a {
+            text-decoration: none;
+            color: #4e56e8;
+            &:after {
+              margin-left: 0.15em;
+              font-weight: 400;
+              content: "↗️";
+            }
+            &:hover {
+              text-decoration: underline;
+              text-underline-offset: 5px;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+@keyframes fadein {
+  0% {
+    opacity: 0;
+    transform: translateY(20px);
+  }
+  100% {
+    opacity: 1;
+    transform: none;
   }
 }
 </style>
