552358914f28951e420d79e63ce22de7b10307d2
이충재 <knm8643@nate.com>
2024-02-18 20:25:23 +0900

    애니메이션 추가 및 전체적인 문구 추가

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -48,25 +48,38 @@
         <div class="row-right">
           <h3>모니모마케팅팀</h3>
           <ul>
-            <li>삼성카드 모니모 레이아웃 신규생성 및 구축</li>
+            <li>
+              뷰티파이 및 SCSS를 활용한 삼성카드 모니모 레이아웃 신규생성 및
+              구축
+            </li>
+            <li>삼성카드 내부 WCMS를 통한 CSS 수정 및 탬플릿 양산</li>
           </ul>
         </div>
       </section>
       <section>
         <div class="row-left">
           <h2>트라이씨클<em>LF.</em></h2>
-          <p>Frontend Developer</p>
+          <p>Web Developer</p>
           <p>2022.04 - 2023.05</p>
           <p>온라인쇼핑몰 개발/ 운영</p>
         </div>
         <div class="row-right">
           <h3>IT개발본부 프론트개발</h3>
           <ul>
-            <li>자사몰(하프클럽, 보리보리) 서비스 고도화 및 개선작업</li>
-            <li>쇼핑몰 서비스에서 구동되는 내부 api 및 웹개발</li>
-            <li>커머스플랫폼 기반의 비즈니스 로직 구현 및 성능 최적화</li>
-            <li>프레임워크 구조 변경 (AngularJS → Vue3)</li>
-            <li>구글애널리스틱스 코드집계</li>
+            <li>
+              AngularJS을 이용한 자사 온라인쇼핑몰 신규기능 및 유지보수
+              프론트개발
+            </li>
+            <li>
+              프론트 개발과 동시에 Java, Spring, Mysql을 통한 데이터 저장까지
+              백단영역 개발 진행
+            </li>
+            <li>
+              vue2와 Java, Spring, Mysql 를 기반으로한 자사 내부인트라넷
+              서비스BO 신규게시판 영역 생성 및 유지보수 진행
+            </li>
+            <li>구프레임워크 마이그레이션 변경 AngularJS → Vue3</li>
+            <li>구글 애널리틱스 집계 결과를 자사 서비스 디비에 적재</li>
           </ul>
         </div>
       </section>
@@ -80,9 +93,9 @@
           <h3>개발 전문 교육 과정</h3>
           <ul>
             <li>프로젝트 리딩 및 다양한 팀과의 협업 경험</li>
-            <li>HTML, CSS, JAVASCRIPT,JQUERY</li>
+            <li>javascript, JQUERY</li>
             <li>
-              Java, Tomcat, Spring, Oracle, JSP 등과 기초적인 IT 지식 교육
+              Java, Tomcat, Spring, Oracle, JSP, css 등과 기초적인 IT 지식 교육
             </li>
           </ul>
         </div>
@@ -128,30 +141,57 @@
           <h2>WebSkil<em>.</em></h2>
           <ul>
             <li>
-              <h4>Java, Spring, mySql, oracle</h4>
+              <h4>Java, Spring, Jsp, mySql, Oracle</h4>
               <p>
                 &middot; LF트라이씨클에서 근무하면서, 이커머스 특성 상 대용량
                 트래픽을 다뤄보는 경험을 쌓았습니다. 이를 통해 자바와 스프링
                 등의 기초적인 백엔드 업무를 전문적으로 수행해왔습니다.
               </p>
               <p>
+                &middot; LF트라이씨클에서 프론트업무와 동시에 서버까지 데이터를
+                저장하는 풀스택개발 경험을 했었습니다
+              </p>
+              <p>
                 &middot; KH정보교육원에서는 기초적인 MVC 패턴을 기반으로 한 CRUD
-                및 SSR을 통한 웹 개발 흐름을 습득하고 학습하였습니다.
+                및 JSP을 통한 웹 개발 흐름을 습득하고 학습하였습니다.
               </p>
             </li>
             <li>
-              <h4>AngularJS, Vue3</h4>
+              <h4>AngularJS, Vue, typescript</h4>
               <p>
-                &middot; 주로 Angular를 사용하여 회사내 프로젝트를
-                개발하였습니다.
+                &middot; 주로 Angular를 사용하여 트라이씨클 자사몰(하프클럽,
+                보리보리)의 모바일 웹과 PC영역들을 개발했었습니다.
               </p>
-              <p>&middot; 앵귤러에서 뷰로 마이그레이션 경험이 있습니다.</p>
+              <p>
+                &middot; 자사인트라넷 Bo에서 Vue를 통한 신규게시판 기능들을
+                개발하였으며 버그들을 수정해왔습니다.
+              </p>
+              <p>
+                &middot; 개인적인 실무경험들과 공부들을 통하여 프론트개발의
+                기술을 키우고자 타입스크립트를 주로 공부하였습니다.
+              </p>
             </li>
             <li>
-              <h4>SCSS</h4>
+              <h4>Jenkins, aws</h4>
               <p>
-                &middot; 스페이드컴퍼니에서 현재 퍼블리셔로 scss를
-                사용하고있습니다.
+                &middot; 트라이씨클에서 주된 서버배포용으로 사용하였으며 개발 및
+                운영서버를 나누어 주된 빌드와 배포를 하였습니다.
+              </p>
+              <p>
+                &middot; 학원프로젝트 진행시 포트폴리오 사이트 공개를 위해 aws
+                사이트 배포를 경험했었습니다.
+              </p>
+            </li>
+            <li>
+              <h4>SCSS, css, Vuetify, bootstrap</h4>
+              <p>
+                &middot; 삼성카드 모니모앱 프로젝트에 퍼블리셔로서 scss 와
+                뷰티파이를 사용한 경험이있습니다.
+              </p>
+              <p>
+                &middot; KH정보교육원에서는 정적인 html영역과 그를 채워주는
+                css에 대해서 기초적인 역량을 배웠으며 부트스트랩을 통한
+                라이브러리 활용경험을 배웠었습니다.
               </p>
             </li>
           </ul>
@@ -162,18 +202,27 @@
           <h2>Collaboration tool<em>.</em></h2>
           <ul>
             <li>
-              <h4>GitHub, GitLab</h4>
+              <h4>GitHub, GitLab, Git, bitbucket</h4>
               <p>
-                &middot; 협업 및 버전 관리를 효율적으로 수행할 수 있습니다. 오픈
-                소스 프로젝트에 참여한 경험이 있어, 다양한 개발자들과의 협업에
-                능숙합니다.
+                &middot; 학원 및 개인적인 프로젝트와 공부등을 위해 git과
+                github를 주로 사용하여 깃을 활용한 협업에 능숙합니다.
+              </p>
+              <p>
+                &middot; 트라이씨클 및 삼성카드 모니모 프로젝트를 깃랩과
+                빗버킷을 활용하여 팀원들과 협업을 진행하였습니다.
               </p>
             </li>
             <li>
               <h4>Jira, redmine, zeplin</h4>
               <p>
-                &middot; 스페이드컴퍼니에서 현재 퍼블리셔로 디자이너들과
-                사용하고있습니다
+                &middot; 트라이씨클에서 개발자 및 MD 들과 소통을 하기위해
+                레드마인을 사용하였으며 삼성카드 모니모에선 디자이너와 현업들과
+                소통하기 위해 지라를 사용했습니다.
+              </p>
+              <p>
+                &middot; 삼성카드 모니모 레이아웃을 구성하기 위해 디자이너들과
+                협업을 위한 제플린을 직접 이용하여 px 값등을 확인하여 완벽한
+                ui/ux를 구성하기 위해 작업했었습니다.
               </p>
             </li>
           </ul>
@@ -186,7 +235,7 @@
         <div class="row-left">
           <h2>집게사장 피하기</h2>
           <p>&middot; 제작기간 : 3일</p>
-          <p>&middot; 진행상황 : 60%</p>
+          <p>&middot; 진행상황 : 50%</p>
           <p>&middot; 주요내용 : 총알피하기를 모티브로 한 게임입니다</p>
         </div>
         <router-link to="/pacman" class="portfolio-link">
