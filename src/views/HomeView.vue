98657323d425690407cb34f896a864e0d5d95b00
이충재 <knm8643@nate.com>
2024-04-17 15:50:41 +0900

    메인타이틀 변경

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="home">
-    <HelloWorld />
+    <!--    <HelloWorld />-->
+    <HelloWorld2></HelloWorld2>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import HelloWorld2 from "@/components/HelloWorldVer2.vue";
 
 @Options({
   components: {
     HelloWorld,
+    HelloWorld2,
   },
 })
 export default class HomeView extends Vue {}
